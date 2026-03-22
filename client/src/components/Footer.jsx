cf6fe1bd287920c21eeee09baba95b1eb6135e25
usanthiya <santhiyaudhya1@gmail.com>
2026-03-22 11:17:41 +0530

    Added Footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { assets, footer_data } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/3">
+      <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500">
+        <div>
+          <img src={assets.logo} alt="logo" className="w-32 sm:w-44" />
+          <p className="max-w-[410px] mt-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+            pariatur sit fuga maiores consectetur fugit repellendus quas hic ut
+            Quaerat quo officia dolorum non eos eum rerum, quia quae cupiditate
+          </p>
+        </div>
+        <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
+            {footer_data.map((section, index)=> (
+                <div key={index}>
+                     <h1 className="font-semibold text-base text-gray-900 md:mb-4 mb-2">{section.title}</h1>
+                     <ul className="text-sm space-y-1">
+                        {section.links.map((link, index)=> (
+                            <li key={index}>
+                                <a href='#' className="hover:underline tranistion">{link}</a>
+                            </li>
+                        ))}
+                     </ul>
+                </div>
+            ))}
+        </div>
+      </div>
+      <p className="py-4 text-center text-sm md:text-base text-gray-500">
+        Copyright @2026 QuickBlog- All rights reserved
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import BlogList from "../components/BlogList";
 import NewsLetter from "../components/NewsLetter";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Header />
       <BlogList />
       <NewsLetter />
+      <Footer />
     </>
   );
 };
