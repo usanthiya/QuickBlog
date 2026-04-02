53fcb038e17e5a2815d984cdd2683cb73a86a6d0
usanthiya <santhiyaudhya1@gmail.com>
2026-04-02 16:00:31 +0530

    Admin dashboard component added

---
+++ client/src/components/admin/BlogTableItem.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { assets } from "../../assets/assets";
+
+const BlogTableItem = ({ blog, fetchBlogs, index }) => {
+  const { title, createdAt } = blog;
+  const BlogDate = new Date(createdAt);
+
+  return (
+   <tr className="border-y border-gray-300">
+        <th className="px-2 py-4">{index}</th>
+        <td className="px-2 py-4">{title}</td>
+        <td className="px-2 py-4">{BlogDate.toDateString()}</td>
+        <td className="px-2 py-4">
+            <p className={`${blog.isPublished ? "text-green-600" : "text-orange-700"}`}>
+                {blog.isPublished ? "Published" : "Unpublished"}
+            </p>
+        </td>
+        <td className="px-2 py-4 flex text-xs gap-3">
+            <button className="border px-2 py-0.5 mt-1 rounded cursor-pointer">
+                {blog.isPublished ? "Unpublish" : "Publish"}
+            </button>
+            <img src={assets.cross_icon} className="w-4 hover:scale-110 transition-all cursor-pointer" alt="Delete" />
+        </td>
+    </tr>
+  ) 
+};
+
+export default BlogTableItem;
--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { assets, dashboard_data } from "../../assets/assets";
+import BlogTableItem from "../../components/admin/BlogTableItem";
 
 const Dashboard = () => {
 
@@ -7,7 +8,7 @@ const Dashboard = () => {
     blogs: 0,
     comments: 0,
     drafts: 0,
-    recentBlpgs: [],
+    recentBlogs: [],
   });
 
   const fetchDashboardData = () => {
@@ -51,6 +52,25 @@ const Dashboard = () => {
         <div className="flex items-center gap-3 m-4 mt-6 text-gray-600">
           <img src={assets.dashboard_icon_4} alt="" />
           <p>Latest Blogs</p>
+        </div>
+
+        <div className="relative max-w-4xl overflow-x-auto shadow rounded-lg scrollbar-hide bg-white">
+           <table className="w-full text-sm text-gray-500">
+            <thead className="text-xs text-gray-600 text-left uppercase">
+               <tr>
+                <th scope="col" className="px-2 py-4 xl:px-6"> # </th>
+                <th scope="col" className="px-2 py-4"> Blog Title </th>
+                <th scope="col" className="px-2 py-4 max-sm:hidden"> Date </th>
+                <th scope="col" className="px-2 py-4 max-sm:hidden"> Status </th>
+                <th scope="col" className="px-2 py-4"> Actions </th>
+               </tr>
+            </thead>
+            <tbody>
+              {dashboardData.recentBlogs.map((blog, index)=> {
+                return <BlogTableItem index={index+1} key={blog._id} blog={blog} fetchBlogs={fetchDashboardData}/>
+              })}
+            </tbody>
+           </table>
         </div>
       </div>
     </div>
