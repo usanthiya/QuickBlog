a7050ea9d057054deb4c7cfc6af1995cb57a4a34
usanthiya <santhiyaudhya1@gmail.com>
2026-04-12 11:26:40 +0530

    feat: Blog CRUD api's added

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,61 +1,141 @@
-import fs from 'fs';
-import imagekit from '../config/imageKit.js';
-import blogModel from '../models/blogModel.js';
+import fs from "fs";
+import imagekit from "../config/imageKit.js";
+import blogModel from "../models/blogModel.js";
 
 export const addBlog = async (req, res) => {
-    const result ={
-        sucess: true,
-        message: "Blog added successfully",
+  const result = {
+    sucess: true,
+    message: "Blog added successfully",
+    data: null,
+  };
+
+  try {
+    const { title, subTitle, description, category, isPublished } = JSON.parse(
+      req.body.blog,
+    );
+    const imageFile = req.file;
+
+    if (!title || !description || !category || !imageFile) {
+      throw new Error("Missing required fields");
+    }
+
+    const fileBuffer = fs.readFileSync(imageFile.path);
+    const fileName = `${imageFile.originalname}_${Date.now()}`;
+
+    // Upload image to ImageKit
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName,
+      folder: "/blogs",
+    });
+
+    //Optimization through imageKit URL transformation
+    const optimizedImageUrl = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        {
+          quality: "auto", // Auto compression
+          format: "webp", // Convert to modern WebP format for better performance
+          width: "1280", //width resizing for faster loading
+        },
+      ],
+    });
+
+    const image = optimizedImageUrl;
+
+    await blogModel.create({
+      title,
+      subTitle,
+      description,
+      category,
+      image,
+      isPublished,
+    });
+
+    res.json(result);
+  } catch (error) {
+    console.log("Error in add blog", error.message);
+    res.json({
+      sucess: false,
+      message: error.message,
+    });
+  }
+};
+
+export const getAllBlogs = async (req, res) => {
+  const result = {
+    success: true,
+    message: "Blogs listed successfully",
+    data: null,
+  };
+  try {
+    const blogs = await blogModel.find({ isPublished: true });
+    result.data = blogs;
+    return res.json(result);
+  } catch (error) {
+    console.error("Error listing blogs: ", getAllBlogs);
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+export const getBlogById = async (req, res) => {
+  const result = {
+    success: true,
+    message: "Blog By Id returned successfully",
+    data: null,
+  };
+  try {
+    const { id } = req.params;
+
+    const blog = await blogModel.findById(id);
+
+    if (!blog) {
+      throw new Error("Blog not Found!");
+    }
+
+    result.data = blog;
+    return res.json(result);
+  } catch (error) {
+    console.error("Error getting blog: ", error);
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+export const deleteBlogById = async (req, res) => {
+  const result = {
+    success: true,
+    message: "Blog deleted successfully",
+    data: null,
+  };
+  try {
+    const { id } = req.params;
+    const response = await blogModel.findByIdAndDelete(id);
+    console.log("blog response: ", response)
+    if (!response) {
+      throw new Error("Blog not found");
+    }
+    return res.json(result);
+  } catch (error) {
+    console.error("Error deleting blog: ", error);
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+export const togglePublish = async(req, res) => {
+    const result = {
+        success: true,
+        message: "",
         data: null
     }
-    console.log("req user: ", req.user)
     try{
-       const { title, subTitle, description, category, isPublished } = JSON.parse(req.body.blog);
-       const imageFile = req.file;
-
-       if(!title || !description || !category || !imageFile){
-        throw new Error("Missing required fields");
-       }
-
-       const fileBuffer = fs.readFileSync(imageFile.path);
-       const fileName = `${imageFile.originalname}_${Date.now()}`;
-
-       // Upload image to ImageKit
-       const response = await imagekit.upload({
-        file: fileBuffer,
-        fileName,
-        folder: "/blogs"
-       })
-
-       //Optimization through imageKit URL transformation
-       const optimizedImageUrl = imagekit.url({
-         path: response.filePath,
-         transformation: [
-            {
-                quality: "auto", // Auto compression
-                format: "webp", // Convert to modern WebP format for better performance
-                width: "1280" //width resizing for faster loading
-            }
-         ]
-       });
-
-       const image = optimizedImageUrl;
-
-       await blogModel.create({
-        title,
-        subTitle,
-        description,
-        category,
-        image,
-        isPublished
-       });
-
-       res.json(result);
+        const { id, isPublished } = req.body;
+        const blog = await blogModel.findOneAndUpdate({ _id: id }, { isPublished }, { new: true });
+        console.log("updated blog: ", blog)
+        { isPublished == true ? result.message = "Blog published successfully": result.message = "Blog unpublished successfully"}
+        result.data = blog;
+        return res.json(result);
     }catch(error){
-        console.log("Error in add blog", error.message);
-        res.json({ 
-            sucess: false,
-            message: error.message 
-        });
+        console.error("Error in publish toggle: ", error);
+        return res.json({ success: false, message: error.message});
     }
 }
--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { addBlog } from '../controllers/blogController.js';
+import { addBlog, deleteBlogById, getAllBlogs, getBlogById, togglePublish } from '../controllers/blogController.js';
 import upload from '../middleware/multer.js';
 import auth from '../middleware/auth.js';
 const router = express.Router();
 
 router.route('/add').post(auth, upload.single('image'), addBlog);
+router.route('/').get(auth, getAllBlogs);
+router.route('/:id').get(auth, getBlogById);
+router.route('/:id').delete(auth, deleteBlogById);
+router.route('/toggle-publish').post(auth, togglePublish);
 
 export default router;
